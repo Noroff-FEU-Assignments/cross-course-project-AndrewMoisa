f3450b4d915fca71d49439cb316eb2b5da1f90d6
AndrewMoisa <113123678+AndrewMoisa@users.noreply.github.com>
2024-02-07 20:28:43 +0100

    Shopping cart quantity fixed + added a new feature to select size of the product

--- js/helpers/calculateTotal.js
+++
@@ -1,8 +0,0 @@
-export default function calculateTotal(cart) {
-  let total = 0;
-  cart.forEach((item) => {
-    total += parseFloat(item.price);
-  });
-  const roundedTotal = total.toFixed(2);
-  return parseFloat(roundedTotal);
-}
--- js/helpers/getQueryParam.js
+++
@@ -1,9 +0,0 @@
-function getQueryParam(param) {
-  const queryString = document.location.search;
-  const params = new URLSearchParams(queryString);
-  const value = params.get(param);
-  console.log(value);
-  return value;
-}
-
-export default getQueryParam;
